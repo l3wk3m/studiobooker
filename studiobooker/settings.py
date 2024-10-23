57d9e14424723a40c02b4d165a6b0202133a1942
Luke Maycock <maycock.luke@gmail.com>
2024-10-23 10:07:40 +0000

    Heroku deployment cont

--- studiobooker/settings.py
+++ studiobooker/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+if os.path.exists("env.py"):
+    import env
+import dj_database_url
 from pathlib import Path
 import cloudinary.api
 import cloudinary
@@ -106,12 +109,17 @@ WSGI_APPLICATION = 'studiobooker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
