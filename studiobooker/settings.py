351945d415016e61440a0c08fb293a8377df61cc
Luke Maycock <maycock.luke@gmail.com>
2024-10-23 10:11:11 +0000

    Set up new secret key as config var

--- studiobooker/settings.py
+++ studiobooker/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d^3-v$jm=&f=&u&__iogvao=02$&cj@0&l0s$cpik3p9b9+s6m'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
